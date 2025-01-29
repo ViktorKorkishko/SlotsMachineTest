422e0d0db10f6827c319adf1e73ad99e305a5f81
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2025-01-29 23:43:34 +0200

    main file

---
+++ SlotMachineTests/SlotMachineTests/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 
+            // tape = [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+            // reel = [ -1, -1, -1, -1, -1 ]
+            //
+            // shift = 3,
+            // spot = 3
+            // reel = [ 5, 4, 3 ]
+            //
+            // shift = 6
+            // spot = 3
+            // reel = [ 0, 7, 6 ]
+            
+            // var tape = new[] { 0, 1, 2, 3, 4, 5, 6, 7 }; // 8
+            // var reel1 = SlotOperations.FillReel(tape, 3, 3);
+            // var reel2 = SlotOperations.FillReel(tape, 3, 6);
+            
+            // Console.WriteLine(string.Join(',', reel1));
+            // Console.WriteLine(string.Join(',', reel2));
+            // Console.ReadLine();
+            
+            // reel = [ body, legs, 0, head, body ] // 2
+            // reel = [ 0, 1, 0, head, body ] // 1
+            // reel = [ 0, head, body, legs, 2 ] // 1
+            // reel = [ legs, 1, 4, 6, 2 ] // 0
+
+            int bigSymbolId = 88;
+            var reel1 = new[] { 88, 88,  0, 88, 88 };
+            var reel2 = new[] {  0,  1,  0, 88, 88 };
+            var reel3 = new[] {  0, 88, 88, 88,  2 };
+            var reel4 = new[] { 88,  1,  4,  6,  2 };
+            
+            Console.WriteLine(SlotOperations.GetBigSymbolsCount(reel1, bigSymbolId)); // 2
+            Console.WriteLine(SlotOperations.GetBigSymbolsCount(reel2, bigSymbolId)); // 1
+            Console.WriteLine(SlotOperations.GetBigSymbolsCount(reel3, bigSymbolId)); // 1
+            Console.WriteLine(SlotOperations.GetBigSymbolsCount(reel4, bigSymbolId)); // 0
+            
+            var bigSymbolParts = new[] { "head", "body", "legs" };
+            var parts1 = SlotOperations.DefineBigSymbolsParts(reel1, bigSymbolId, bigSymbolParts);
+            var parts2 = SlotOperations.DefineBigSymbolsParts(reel2, bigSymbolId, bigSymbolParts);
+            var parts3 = SlotOperations.DefineBigSymbolsParts(reel3, bigSymbolId, bigSymbolParts);
+            var parts4 = SlotOperations.DefineBigSymbolsParts(reel4, bigSymbolId, bigSymbolParts);
+            
+            var reel5 = new[] { 88, 88,  0, 88, 88, 88 };
+            var reel6 = new[] {  0,  1,  0, 88, 88, 88 };
+            var reel7 = new[] {  0, 88, 88, 88, 88,  2 };
+            var reel8 = new[] { 88,  1,  4,  6,  2, 88 };
+            
+            bigSymbolParts = new[] { "head", "body1", "body2", "legs" };
+            var parts5 = SlotOperations.DefineBigSymbolsParts(reel5, bigSymbolId, bigSymbolParts);
+            var parts6 = SlotOperations.DefineBigSymbolsParts(reel6, bigSymbolId, bigSymbolParts);
+            var parts7 = SlotOperations.DefineBigSymbolsParts(reel7, bigSymbolId, bigSymbolParts);
+            var parts8 = SlotOperations.DefineBigSymbolsParts(reel8, bigSymbolId, bigSymbolParts);
+            
+            
+        }
+    }
+}
---
+++ SlotMachineTests/SlotMachineTests/SlotOperations.cs
@@ -0,0 +1,110 @@
+﻿namespace Test
+
+{
+    public static class SlotOperations
+    {
+        public static int[] FillReel(int[] tape, int spot, int shift)
+        {
+            int[] result = new int[spot];
+            int startIndex = spot + shift - 1;
+            if (startIndex >= tape.Length)
+            {
+                startIndex -= tape.Length;
+            }
+
+            for (int i = 0; i < spot; i++)
+            {
+                int tapeIndex = startIndex - i;
+                
+                if (tapeIndex < 0)
+                {
+                    tapeIndex = tape.Length - i;
+                }
+                
+                var tapeElement = tape[tapeIndex];
+                result[i] = tapeElement;
+            }
+            
+            return result;
+        }
+        
+        public static int GetBigSymbolsCount(int[] reel, int bigSymbolId)
+        {
+            int bigSymbolsCount = 0;
+            
+            for (int i = 1; i < reel.Length - 1; i++)
+            {
+                var spotElement = reel[i];
+                if (spotElement == bigSymbolId)
+                {
+                    bigSymbolsCount++;
+                }
+            }
+            
+            if (bigSymbolsCount == reel.Length - 2)
+            {
+                return 1;
+            }
+            
+            return bigSymbolsCount;
+        }
+        
+        public static string[] DefineBigSymbolsParts(int[] reel, int bigSymbolId, string[] bigSymbolParts)
+        {
+            var result = new string[reel.Length];
+
+            int nextSymbolIndex = 1;
+            int bigSymbolsCountInARow = 0;
+            for (int i = 0; i < reel.Length; i++)
+            {
+                var elementId = reel[i];
+                if (elementId == bigSymbolId)
+                {
+                    bigSymbolsCountInARow++;
+
+                    if (reel.IsInRange(nextSymbolIndex))
+                    {
+                        if (reel[nextSymbolIndex] != bigSymbolId)
+                        {
+                            CompleteSymbol(i);
+                        }
+                    }
+                    else
+                    {
+                        CompleteSymbol(i);
+                    }
+                }
+                else
+                {
+                    result[i] = elementId.ToString();
+                    bigSymbolsCountInARow = 0;
+                }
+                
+                nextSymbolIndex++;
+            }
+
+            return result;
+
+            void CompleteSymbol(int currentIndex)
+            {
+                int startIndex = 0;
+                if (currentIndex < bigSymbolsCountInARow)
+                {
+                    startIndex = bigSymbolParts.Length - bigSymbolsCountInARow;
+                }
+                
+                for (int j = 0; j < bigSymbolsCountInARow; j++)
+                {
+                    var bigSymbolPart = bigSymbolParts[startIndex + j];
+                    result[currentIndex - bigSymbolsCountInARow + j + 1] = bigSymbolPart;
+                }
+            }
+        }
+
+        public static bool IsInRange(this int[] array, int i)
+        {
+            bool inRange = i >= 0 && i < array.Length;
+            return inRange;
+        }
+    }
+}
